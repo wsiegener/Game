5f4904b3f7a43080ffa70d985634c6de475efe32
Billy Siegener <williamsiegener@duxbury.k12.ma.us>
2016-05-05 12:54:08 -0400

    Initial commit

---
+++ desktop/src/com/siggy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.siggy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.siggy.game.Game;
+
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
